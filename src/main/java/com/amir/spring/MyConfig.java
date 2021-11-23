d057bcd639da8f8969592b5917c3cb41885a8018
amirkenesbay <amirkenesbay@gmail.com>
2021-11-23 20:47:49 +0600

    Change MyConfig and Test4 classes

--- src/main/java/com/amir/spring/MyConfig.java
+++ src/main/java/com/amir/spring/MyConfig.java
@@ -3,12 +3,24 @@ package com.amir.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan("com.amir.spring")
+//@ComponentScan("com.amir.spring")
 public class MyConfig {
     @Bean
+    @Scope("prototype")
     public Pet catBean(){
         return new Cat();
+    }
+
+    @Bean
+    public Person personBean(){
+        return new Person(catBean());
+    }
+
+    @Bean
+    public Person personBean2(){
+        return new Person(catBean());
     }
 }
--- src/main/java/com/amir/spring/Test4.java
+++ src/main/java/com/amir/spring/Test4.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test4 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
         Person person = context.getBean("personBean", Person.class);
+        Person person2 = context.getBean("personBean", Person.class);
         person.callYourPet();
         context.close();
     }
