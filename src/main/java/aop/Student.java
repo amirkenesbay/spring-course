0c2d7454f23623125990620bea5f99b42a2a7fab
amirkenesbay <amirkenesbay@gmail.com>
2021-12-08 18:29:48 +0600

    Add AfterReturning pointcut

---
+++ src/main/java/aop/Student.java
@@ -0,0 +1,46 @@
+package aop;
+
+public class Student {
+    private String nameSurname;
+    private int count;
+    private double avgGrade;
+
+    public Student(String nameSurname, int count, double avgGrade) {
+        this.nameSurname = nameSurname;
+        this.count = count;
+        this.avgGrade = avgGrade;
+    }
+
+    public String getNameSurname() {
+        return nameSurname;
+    }
+
+    public void setNameSurname(String nameSurname) {
+        this.nameSurname = nameSurname;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "\nStudent{" +
+                "nameSurname='" + nameSurname + '\'' +
+                ", count=" + count +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
---
+++ src/main/java/aop/University.java
@@ -0,0 +1,26 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class University {
+    private final List<Student> students = new ArrayList<>();
+
+    public void addStudents() {
+        Student st1 = new Student("Amir Kenesbay", 4, 7.5);
+        Student st2 = new Student("John Weak", 4, 7.5);
+        Student st3 = new Student("Michael Adamson", 4, 7.5);
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+    }
+
+    public List<Student> getStudents() {
+        System.out.println("Information from method getStudents: ");
+        System.out.println(students);
+        return students;
+    }
+}
