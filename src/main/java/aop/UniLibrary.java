b1104bca232933e3dd3aa6f225ae4f338d8852a3
amirkenesbay <amirkenesbay@gmail.com>
2021-12-12 00:40:28 +0600

    Add Around

--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -3,33 +3,34 @@ package aop;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UniLibrary extends AbstractLibrary{
-    public void getBook(){
+public class UniLibrary extends AbstractLibrary {
+    public void getBook() {
         System.out.println("Мы берем книгу из UniLibrary");
         System.out.println("-------------------------------------");
     }
 
-    public void returnBook(){
+    public String returnBook() {
+        int a = 10 / 0;
         System.out.println("Мы возвращаем книгу в UniLibrary");
-        System.out.println("-------------------------------------");
+        return "Война и мир";
     }
 
-    public void getMagazine(){
+    public void getMagazine() {
         System.out.println("Мы берем журнал из UniLibrary");
         System.out.println("-------------------------------------");
     }
 
-    public void returnMagazine(){
+    public void returnMagazine() {
         System.out.println("Мы возвращаем журнал в UniLibrary");
         System.out.println("-------------------------------------");
     }
 
-    public void addBook(String person_name, Book book){
+    public void addBook(String person_name, Book book) {
         System.out.println("Мы добавляем книгу в UniLibrary");
         System.out.println("-------------------------------------");
     }
 
-    public void addMagazine(){
+    public void addMagazine() {
         System.out.println("Мы добавляем журнал в UniLibrary");
         System.out.println("-------------------------------------");
     }
---
+++ src/main/java/aop/aspects/NewLoggingAspect.java
@@ -0,0 +1,28 @@
+package aop.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class NewLoggingAspect {
+
+    @Around("execution(public String returnBook())")
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку пытается вернуть книгу");
+
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = proceedingJoinPoint.proceed();
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: было поймано исключение " + e);
+            throw e;
+        }
+
+        System.out.println("aroundReturnBookLoggingAdvice: в библиотеку успешно вернули книгу");
+
+        return targetMethodResult;
+    }
+}
