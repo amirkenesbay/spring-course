ccfd8c93d6a9da639dfed82ddcaea32dda6f855c
Amir <amirkenesbay@gmail.com>
2021-12-22 21:05:29 +0600

    Add changes for new example

--- src/main/java/hibernate_test3/Test1.java
+++ src/main/java/hibernate_test3/Test1.java
@@ -11,19 +11,18 @@ import java.io.File;
 public class Test1 {
     static Session session = null;
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration()
+
+        try (SessionFactory factory = new Configuration()
                 .configure(new File("hibernate.cfg.xml"))
                 .addAnnotatedClass(Person.class)
                 .addAnnotatedClass(Passport.class)
-                .buildSessionFactory();
+                .buildSessionFactory()) {
 
-
-        try {
 //            addPersonAndPassportDetails(session, factory);
             getPersonDetails(factory);
+            getPassportDetails(factory);
         } finally {
             session.close();
-            factory.close();
         }
     }
 
@@ -31,14 +30,10 @@ public class Test1 {
         session = factory.getCurrentSession();
         Person person = new Person("Michael", "Jordan", 45);
         Passport passport = new Passport(2020, 2030);
-
         person.setPassportDetail(passport);
         passport.setPerson(person);
-
         session.beginTransaction();
-
         session.save(passport);
-
         session.getTransaction().commit();
         System.out.println("Done");
         session.close();
@@ -48,8 +43,16 @@ public class Test1 {
         session = factory.getCurrentSession();
         session.beginTransaction();
         Person person = session.get(Person.class, 1);
-        Passport passport = session.get(Passport.class, 1);
         System.out.println(person.getPassportDetail());
+        session.getTransaction().commit();
+        System.out.println("Done");
+        session.close();
+    }
+
+    private static void getPassportDetails(SessionFactory factory){
+        session = factory.getCurrentSession();
+        session.beginTransaction();
+        Passport passport = session.get(Passport.class, 1);
         System.out.println(passport.getPerson());
         session.getTransaction().commit();
         System.out.println("Done");
